9f30561cc85f38cb09dcbcdbe7617801daae926d
d-a-weston <daniel.aaron.weston@gmail.com>
2023-12-02 10:51:51 +1000

    chore: first test

--- src/01/main_test.go
+++ src/01/main_test.go
@@ -4,6 +4,12 @@ import (
 	"testing"
 )
 
-func TestPlaceholder(t *testing.T) {
-	return
+func TestFindNumInLine(t *testing.T) {
+	line := "1abc2"
+	expected := 12
+	actual := findNumInLine(line)
+
+	if actual != expected {
+		t.Errorf("Expected %d, got %d", expected, actual)
+	}
 }
