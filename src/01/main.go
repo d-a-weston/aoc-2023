1ab89f4d3258db29b78762c2db82d380fb945a45
d-a-weston <daniel.aaron.weston@gmail.com>
2023-12-02 11:15:08 +1000

    chore: num inside string passing

--- src/01/main.go
+++ src/01/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 // https://adventofcode.com/2023/day/1
@@ -17,5 +18,29 @@ func main() {
 }
 
 func findNumInLine(line string) int {
-	return 12
+	var first, last int
+
+	for _, char := range line {
+		charInt, err := strconv.Atoi(string(char))
+
+		if err != nil {
+			continue
+		}
+
+		if first == 0 {
+			first = charInt
+		}
+
+		last = charInt
+	}
+
+	numString := fmt.Sprintf("%d%d", first, last)
+
+	num, err := strconv.Atoi(numString)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return num
 }
