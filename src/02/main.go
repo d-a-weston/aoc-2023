a4a853ff7d561cc43c526611e6d6a2620d968d94
d-a-weston <daniel.aaron.weston@gmail.com>
2023-12-02 18:34:03 +1000

    chore: passing test

--- src/02/main.go
+++ src/02/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 // https://adventofcode.com/2023/day/2
@@ -23,5 +25,32 @@ func main() {
 }
 
 func colorMax(line string) map[string]int {
-	return map[string]int{"blue": 6, "red": 4, "green": 2}
+	colors := map[string]int{
+		"blue":  0,
+		"red":   0,
+		"green": 0,
+	}
+
+	trimGames := strings.Split(line, ":")
+	games := strings.Split(trimGames[1], ";")
+
+	for _, game := range games {
+		colorsInGame := strings.Split(game, ",")
+		for _, color := range colorsInGame {
+			color = strings.TrimSpace(color)
+			colorCount := strings.Split(color, " ")
+			colorName := colorCount[1]
+			colorCountInt, err := strconv.Atoi(colorCount[0])
+
+			if err != nil {
+				panic(err)
+			}
+
+			if colorCountInt > colors[colorName] {
+				colors[colorName] = colorCountInt
+			}
+		}
+	}
+
+	return colors
 }
