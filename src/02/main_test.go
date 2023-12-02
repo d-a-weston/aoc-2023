f170e06e1c6ed691f3ae08cc2e7a6961347e303b
d-a-weston <daniel.aaron.weston@gmail.com>
2023-12-02 18:22:16 +1000

    chore: first test case

--- src/02/main_test.go
+++ src/02/main_test.go
@@ -4,6 +4,24 @@ import (
 	"testing"
 )
 
-func TestPlaceholder(t *testing.T) {
-	return
+func TestColorMax(t *testing.T) {
+	var tests = []struct {
+		name     string
+		input    string
+		expected map[string]int
+	}{
+		{"Color max", "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", map[string]int{"blue": 6, "red": 4, "green": 2}},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := colorMax(test.input)
+
+			for color, value := range test.expected {
+				if actual[color] != value {
+					t.Errorf("Expected %d, got %d", value, actual[color])
+				}
+			}
+		})
+	}
 }
