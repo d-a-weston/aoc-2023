ef0f13cb893b1265f032f4b04d86e4f463bdb1e8
d-a-weston <daniel.aaron.weston@gmail.com>
2023-12-02 18:35:51 +1000

    chore: added remaining test cases from aoc

--- src/02/main_test.go
+++ src/02/main_test.go
@@ -12,6 +12,9 @@ func TestColorMax(t *testing.T) {
 	}{
 		{"Color max #1", "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", map[string]int{"blue": 6, "red": 4, "green": 2}},
 		{"Color max #2", "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", map[string]int{"blue": 4, "red": 1, "green": 3}},
+		{"Color max #3", "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", map[string]int{"blue": 6, "red": 20, "green": 13}},
+		{"Color max #4", "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", map[string]int{"blue": 15, "red": 14, "green": 3}},
+		{"Color max #5", "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", map[string]int{"blue": 2, "red": 6, "green": 3}},
 	}
 
 	for _, test := range tests {
