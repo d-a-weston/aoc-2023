da06d79d630e7bb2298c570d39660feffcc7ac05
d-a-weston <daniel.aaron.weston@gmail.com>
2023-12-03 18:33:26 +1000

    chore: more tests and some refactor

--- src/03/main_test.go
+++ src/03/main_test.go
@@ -6,20 +6,22 @@ import (
 
 func TestCheckSurroundingCells(t *testing.T) {
 	tests := []struct {
-		name     string
-		input    [][]string
-		start    int
-		finish   int
-		expected bool
+		name       string
+		input      [][]string
+		start      int
+		finish     int
+		lineNumber int
+		expected   bool
 	}{
-		{"Find single digit number with symbol", [][]string{{".", ".", "."}, {".", "1", "."}, {"*", ".", "."}}, 1, 1, true},
-		{"Find two number digit with symbol", [][]string{{".", ".", ".", "*"}, {".", "1", "2", "."}, {".", ".", ".", "."}}, 1, 2, true},
-		{"Invalid case", [][]string{{".", ".", ".", "*"}, {".", "3", ".", "."}, {".", ".", ".", "."}}, 1, 1, false},
+		{"Find single digit number with symbol", [][]string{{".", ".", "."}, {".", "1", "."}, {"*", ".", "."}}, 1, 1, 1, true},
+		{"Find two number digit with symbol", [][]string{{".", ".", ".", "*"}, {".", "1", "2", "."}, {".", ".", ".", "."}}, 1, 2, 1, true},
+		{"Number on top row", [][]string{{".", "9", "0", "*"}, {".", "*", ".", "."}, {".", ".", ".", "."}}, 1, 2, 0, true},
+		{"Invalid case", [][]string{{".", ".", ".", "*"}, {".", "3", ".", "."}, {".", ".", ".", "."}}, 1, 1, 1, false},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			result := checkSurroundingCells(test.input, test.start, test.finish)
+			result := checkSurroundingCells(test.input, test.start, test.finish, test.lineNumber)
 
 			if result != test.expected {
 				t.Errorf("Expected %v, got %v", test.expected, result)
