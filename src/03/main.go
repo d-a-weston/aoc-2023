f5df77926411d9ecdcc5bfa4751e82704a67db0d
d-a-weston <daniel.aaron.weston@gmail.com>
2023-12-03 16:09:09 +1000

    chore: solved first case

--- src/03/main.go
+++ src/03/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 )
 
 func main() {
@@ -19,4 +20,28 @@ func main() {
 		line := fileScanner.Text()
 		fmt.Println(line)
 	}
+}
+
+func checkSurroundingCells(input [][]string, x int, y int) bool {
+	ignore := []string{".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+
+	for i := x - 1; i <= x+1; i++ {
+		if i < 0 || i >= len(input) {
+			continue
+		}
+
+		for j := y - 1; j <= y+1; j++ {
+			if j < 0 || j >= len(input[i]) {
+				continue
+			}
+
+			if slices.Contains(ignore, input[i][j]) {
+				continue
+			}
+
+			return true
+		}
+	}
+
+	return false
 }
