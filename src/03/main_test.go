11dee0fc7932f78f800e5145b7e848227a77a2b0
d-a-weston <daniel.aaron.weston@gmail.com>
2023-12-03 15:16:29 +1000

    chore: first test case

--- src/03/main_test.go
+++ src/03/main_test.go
@@ -4,6 +4,24 @@ import (
 	"testing"
 )
 
-func TestPlaceHolder(t *testing.T) {
-	return
+func TestCheckSurroundingCells(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    [][]string
+		x        int
+		y        int
+		expected bool
+	}{
+		{"Find single digit with symbol", [][]string{{".", ".", "."}, {".", "1", "."}, {"*", ".", "."}}, 1, 1, true},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := checkSurroundingCells(test.input, test.x, test.y)
+
+			if result != test.expected {
+				t.Errorf("Expected %v, got %v", test.expected, result)
+			}
+		})
+	}
 }
