04950bfbc54e64ba3f8bc1346db323daeb9bb004
d-a-weston <daniel.aaron.weston@gmail.com>
2023-12-07 15:22:32 +1000

    chore: first test complete

--- src/07/main.go
+++ src/07/main.go
@@ -4,7 +4,34 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 )
+
+var handTypes = map[int]string{
+	1: "High Card",
+	2: "One Pair",
+	3: "Two Pair",
+	4: "Three of a Kind",
+	5: "Full House",
+	6: "Four of a Kind",
+	7: "Five of a Kind",
+}
+
+var cardValues = map[string]int{
+	"2": 1,
+	"3": 2,
+	"4": 3,
+	"5": 4,
+	"6": 5,
+	"7": 6,
+	"8": 7,
+	"9": 8,
+	"T": 9,
+	"J": 10,
+	"Q": 11,
+	"K": 12,
+	"A": 13,
+}
 
 func main() {
 	file, _ := os.Open("input.txt")
@@ -18,4 +45,40 @@ func main() {
 	}
 
 	file.Close()
+}
+
+func getHandType(hand string) int {
+	var handValue int
+
+	cardsInHand := map[string]int{}
+
+	for _, card := range hand {
+		cardsInHand[string(card)]++
+	}
+
+	numOfSameCards := []int{}
+
+	for _, value := range cardsInHand {
+		if value > 0 {
+			numOfSameCards = append(numOfSameCards, value)
+		}
+	}
+
+	if slices.Contains(numOfSameCards, 5) {
+		handValue = 7
+	} else if slices.Contains(numOfSameCards, 4) {
+		handValue = 6
+	} else if slices.Contains(numOfSameCards, 3) && slices.Contains(numOfSameCards, 2) {
+		handValue = 5
+	} else if slices.Contains(numOfSameCards, 3) {
+		handValue = 4
+	} else if slices.Contains(numOfSameCards, 2) && len(numOfSameCards) == 3 {
+		handValue = 3
+	} else if slices.Contains(numOfSameCards, 2) {
+		handValue = 2
+	} else {
+		handValue = 1
+	}
+
+	return handValue
 }
--- src/07/main_test.go
+++ src/07/main_test.go
@@ -4,6 +4,28 @@ import (
 	"testing"
 )
 
-func TestPlaceholder(t *testing.T) {
-	return
+func TestGetHandType(t *testing.T) {
+	tests := []struct {
+		name     string
+		hand     string
+		expected int
+	}{
+		{"Gets value of high card", "32TJ5", 1},
+		{"Gets value of one pair", "32T3K", 2},
+		{"Gets value of two pairs", "32T32", 3},
+		{"Gets value of three of a kind", "T55J5", 4},
+		{"Gets value of full house", "555JJ", 5},
+		{"Gets value of four of a kind", "5555J", 6},
+		{"Gets value of five of a kind", "55555", 7},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := getHandType(test.hand)
+
+			if actual != test.expected {
+				t.Errorf("Expected %d, got %d", test.expected, actual)
+			}
+		})
+	}
 }
