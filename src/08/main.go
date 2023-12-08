f7b282a667763e72007da200166f086d12a55cd6
d-a-weston <daniel.aaron.weston@gmail.com>
2023-12-08 15:27:01 +1000

    feat: solved day 08 part 1

--- src/08/main.go
+++ src/08/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 )
 
 func main() {
@@ -11,11 +12,47 @@ func main() {
 
 	fileScanner := bufio.NewScanner(file)
 
+	directions := ""
+	maps := map[string]map[string]string{}
+	lineNumber := 0
+
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
 
-		fmt.Println(line)
+		if lineNumber == 0 {
+			directions = line
+		}
+
+		re := regexp.MustCompile(`[A-Z]{3}`)
+		matches := re.FindAll([]byte(line), -1)
+
+		if len(matches) == 3 {
+			maps[string(matches[0])] = map[string]string{
+				"L": string(matches[1]),
+				"R": string(matches[2]),
+			}
+		}
+
+		lineNumber++
 	}
+
+	steps := 0
+	currentStep := 0
+	currentMap := "AAA"
+
+	for currentMap != "ZZZ" {
+		steps++
+
+		currentMap = maps[currentMap][string(directions[currentStep])]
+
+		if currentStep == len(directions)-1 {
+			currentStep = 0
+		} else {
+			currentStep++
+		}
+	}
+
+	fmt.Println(steps)
 
 	file.Close()
 }
