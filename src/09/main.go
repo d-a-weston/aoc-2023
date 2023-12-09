dbd395e14bc38d8ef6205681932968a9275835e2
d-a-weston <daniel.aaron.weston@gmail.com>
2023-12-09 15:17:27 +1000

    chore: functions implemented

--- src/09/main.go
+++ src/09/main.go
@@ -19,3 +19,30 @@ func main() {
 
 	file.Close()
 }
+
+func sequenceDiff(sequence []int) []int {
+	diffs := []int{}
+
+	for i := 0; i < len(sequence)-1; i++ {
+		diffs = append(diffs, sequence[i+1]-sequence[i])
+	}
+
+	return diffs
+}
+
+func findNextValue(sequence []int) int {
+	if All(sequence, func(x int) bool { return x == 0 }) {
+		return 0
+	}
+
+	return sequence[len(sequence)-1] + findNextValue(sequenceDiff(sequence))
+}
+
+func All[T any](ts []T, pred func(T) bool) bool {
+	for _, t := range ts {
+		if !pred(t) {
+			return false
+		}
+	}
+	return true
+}
--- src/09/main_test.go
+++ src/09/main_test.go
@@ -4,6 +4,54 @@ import (
 	"testing"
 )
 
-func TestPlaceholder(t *testing.T) {
-	return
+func TestSequenceDiff(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    []int
+		expected []int
+	}{
+		{"First example", []int{0, 3, 6, 9, 12, 15}, []int{3, 3, 3, 3, 3}},
+		{"Second example", []int{3, 3, 3, 3, 3}, []int{0, 0, 0, 0}},
+		{"Third example", []int{10, 13, 16, 21, 30, 45}, []int{3, 3, 5, 9, 15}},
+		{"Fourth example", []int{3, 3, 5, 9, 15}, []int{0, 2, 4, 6}},
+		{"Fifth example", []int{0, 2, 4, 6}, []int{2, 2, 2}},
+		{"Sixth example", []int{2, 2, 2}, []int{0, 0}},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := sequenceDiff(test.input)
+
+			for i, v := range actual {
+				if v != test.expected[i] {
+					t.Errorf("Expected %v, got %v", test.expected, actual)
+				}
+			}
+		})
+	}
+}
+
+func TestFindNextValue(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    []int
+		expected int
+	}{
+		{"First example", []int{0, 3, 6, 9, 12, 15}, 18},
+		{"Second example", []int{3, 3, 3, 3, 3}, 3},
+		{"Third example", []int{10, 13, 16, 21, 30, 45}, 68},
+		{"Fourth example", []int{3, 3, 5, 9, 15}, 23},
+		{"Fifth example", []int{0, 2, 4, 6}, 8},
+		{"Sixth example", []int{2, 2, 2}, 2},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := findNextValue(test.input)
+
+			if actual != test.expected {
+				t.Errorf("Expected %v, got %v", test.expected, actual)
+			}
+		})
+	}
 }
