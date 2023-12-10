ee4c53f5d966e7c200ebaf5275e04a16b4f52fab
d-a-weston <daniel.aaron.weston@gmail.com>
2023-12-11 09:32:46 +1000

    feat: solved day 10 part 1

--- src/10/main.go
+++ src/10/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
+	"strings"
 )
 
 func main() {
@@ -11,11 +13,92 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
-	for scanner.Scan() {
-		line := scanner.Text()
+	pipes := [][]string{}
+	startX := 0
+	startY := 0
 
-		fmt.Println(line)
+	lineNum := 0
+
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), "")
+
+		containsStart := slices.Index(line, "S")
+
+		if containsStart != -1 {
+			startX = containsStart
+			startY = lineNum
+		}
+
+		pipes = append(pipes, line)
+
+		lineNum++
 	}
 
+	direction := "down"
+	curX := startX
+	curY := startY
+	steps := 0
+	backToStart := false
+
+	for !backToStart {
+		if direction == "down" {
+			curY++
+		} else if direction == "up" {
+			curY--
+		} else if direction == "left" {
+			curX--
+		} else if direction == "right" {
+			curX++
+		}
+
+		fmt.Printf("curX: %d, curY: %d\n", curX, curY)
+
+		direction = setDirection(pipes[curY][curX], direction)
+
+		steps++
+
+		if pipes[curY][curX] == "S" {
+			backToStart = true
+		}
+	}
+
+	fmt.Println(steps / 2)
+
 	file.Close()
+}
+
+func setDirection(pipe string, currDirection string) string {
+	nextDirection := ""
+
+	if pipe == "|" {
+		nextDirection = currDirection
+	} else if pipe == "-" {
+		nextDirection = currDirection
+	} else if pipe == "F" {
+		if currDirection == "up" {
+			nextDirection = "right"
+		} else if currDirection == "left" {
+			nextDirection = "down"
+		}
+	} else if pipe == "7" {
+		if currDirection == "up" {
+			nextDirection = "left"
+		} else if currDirection == "right" {
+			nextDirection = "down"
+		}
+	} else if pipe == "L" {
+		if currDirection == "down" {
+			nextDirection = "right"
+		} else if currDirection == "left" {
+			nextDirection = "up"
+		}
+	} else if pipe == "J" {
+		if currDirection == "down" {
+			nextDirection = "left"
+		} else if currDirection == "right" {
+			nextDirection = "up"
+		}
+	}
+
+	return nextDirection
 }
