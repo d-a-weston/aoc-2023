2f4c02910221954abd09fbd95e9fc1cba9b5d231
d-a-weston <daniel.aaron.weston@gmail.com>
2023-12-12 20:16:05 +1000

    feat: solved day 12 part 1

--- src/12/main.go
+++ src/12/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -11,11 +13,60 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
+	total := 0
+
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		fmt.Println(line)
+		parts := strings.Split(line, " ")
+
+		config := parts[0]
+
+		groups := []int{}
+
+		for _, group := range strings.Split(parts[1], ",") {
+			groupAsInt, _ := strconv.Atoi(group)
+			groups = append(groups, groupAsInt)
+		}
+
+		total += numPossibleConfigurations(config, groups)
 	}
 
+	fmt.Println(total)
+
 	file.Close()
+}
+
+func numPossibleConfigurations(config string, groups []int) int {
+	if config == "" {
+		if len(groups) == 0 {
+			return 1
+		}
+		return 0
+	}
+
+	if len(groups) == 0 {
+		if strings.Contains(config, "#") {
+			return 0
+		}
+		return 1
+	}
+
+	result := 0
+
+	if config[0] == '.' || config[0] == '?' {
+		result += numPossibleConfigurations(config[1:], groups)
+	}
+
+	if config[0] == '#' || config[0] == '?' {
+		if groups[0] <= len(config) && !strings.Contains(config[:groups[0]], ".") && (groups[0] == len(config) || config[groups[0]] != '#') {
+			if groups[0] == len(config) {
+				result += numPossibleConfigurations("", groups[1:])
+			} else {
+				result += numPossibleConfigurations(config[groups[0]+1:], groups[1:])
+			}
+		}
+	}
+
+	return result
 }
