cebcfaa9d209256d103fe0abbbb3aa7a7453fe60
d-a-weston <daniel.aaron.weston@gmail.com>
2023-12-14 15:49:37 +1000

    feat: solved day 14 part 1

--- src/14/main.go
+++ src/14/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -11,11 +12,85 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
+	board := [][]string{}
+
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		fmt.Println(line)
+		board = append(board, strings.Split(line, ""))
 	}
 
+	tiltedBoard := tiltBoard(board, "north")
+	total := 0
+
+	for i, row := range tiltedBoard {
+		numStones := 0
+
+		for _, col := range row {
+			if col == "O" {
+				numStones++
+			}
+		}
+
+		total += numStones * (len(tiltedBoard) - i)
+
+		fmt.Println(row)
+	}
+
+	fmt.Println(total)
+
 	file.Close()
+}
+
+func tiltBoard(board [][]string, direction string) [][]string {
+	tiltedBoard := [][]string{}
+
+	for i := 0; i < len(board); i++ {
+		tiltedBoard = append(tiltedBoard, []string{})
+	}
+
+	if direction == "north" {
+		for i := 0; i < len(board[0]); i++ {
+			tiltedSlice := tiltSlice(getColumnSlice(board, i))
+
+			for j := 0; j < len(tiltedSlice); j++ {
+				tiltedBoard[j] = append(tiltedBoard[j], tiltedSlice[j])
+			}
+		}
+	}
+
+	return tiltedBoard
+}
+
+func tiltSlice(slice []string) []string {
+	i := 0
+	j := 0
+
+	for j < len(slice) {
+		if slice[j] == "O" {
+			slice[i] = "O"
+
+			if i != j {
+				slice[j] = "."
+			}
+
+			i++
+		} else if slice[j] == "#" {
+			i = j + 1
+		}
+
+		j++
+	}
+
+	return slice
+}
+
+func getColumnSlice(pattern [][]string, col int) []string {
+	columnSlice := []string{}
+
+	for _, row := range pattern {
+		columnSlice = append(columnSlice, row[col])
+	}
+
+	return columnSlice
 }
